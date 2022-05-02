9dd6394fbfe27e5a336616e25bfdebb8608b7210
Maria <pashkovichma@gmail.com>
2022-05-02 05:39:36 +0400

    feat: add binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -58,19 +58,48 @@ class BinarySearchTree {
     return null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data, node = this._root) {
+    if (node === null) {
+      return null;
+    } else if (data < node.data) {// данные для удаления меньше, переходим влево
+        node.left = this.remove(data, node.left);
+      return node;
+    } else if (data > node.data) {//данные для удаления больше, переходим вправа
+        node.right = this.remove(data, node.right);
+      return node;
+    } else {//данные равны корню, удаляем узел
+        if (node.right === null && node.left === null){//у узла нет потомков
+          node = null;
+          return node;
+        }
+        if (node.left === null){//у узла один потомок
+          node = node.right;
+          return node;
+        } else if (node.right === null){
+          node = node.left;
+          return node;
+        }
+        let newNode = this.min(node.right);//у узла 2 потомка
+        node.data = newNode;
+        node.right = this.remove(newNode, node.right);
+        return node;
+    }
   }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  min(node = this._root) {
+    if (node.left === null){
+      return node.data;
+    } else {
+      return this.min(node.left);
+    }
   }
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  max(node = this._root) {
+    if (node.right === null){
+      return node.data;
+    } else {
+      return this.max(node.right);
+    }
   }
 }
 
